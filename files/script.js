c6a02b9bbae98f8248fe7903e87d97880cee55e2
Erdemhso <erdemhso@mail.ru>
2020-11-17 16:15:15 +0300

    up

---
+++ files/script.js
@@ -0,0 +1,1 @@
+$(window).on("load",function(){$(".preloader").fadeOut("slow");});const doFade=()=>{$(`#copyied`).fadeIn("slow").delay(600).fadeOut("slow")};function goBack(){window.history.back();}
